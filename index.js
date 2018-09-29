241e2a8c188881cdb5c5766c0c18df39541ca8d8
Talisa Erazo <talisaerazo@gmail.com>
2018-09-29 18:13:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,28 +5,13 @@ function theBeatlesPlay(musicians, instruments) {
   }
     return allMusicians;
 }
-function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
-  var newFacts = [];
+	
+function johnLennonFacts(facts) {
+  var array = [];
   let i = 0;
-    while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!"); i++;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
   }
-  return newFacts;
-}
-function johnLennonFacts() {
-  var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
-  let newFacts = [];
-  var i = 0;
-    while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!"); i++;
-  }
-  return newFacts;
+  return array
 }
