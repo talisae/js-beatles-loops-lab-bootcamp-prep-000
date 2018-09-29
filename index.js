90a048c842550556043d5433069bb4291d5aa020
Talisa Erazo <talisaerazo@gmail.com>
2018-09-29 18:08:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ function johnLennonFacts() {
   }
   return newFacts;
 }
+function johnLennonFacts() {
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  let newFacts = [];
+  var i = 0;
+    while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!"); i++;
+  }
+  return newFacts;
+}
